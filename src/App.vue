8bdb1292cd0aae1db148c6a6991f6909714cc582
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-11-07 15:48:41 +0100

    added main component and added db.json

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script>
 import PageHeader from './components/PageHeader.vue';
-import Product from './components/Product.vue';
+import MainContent from './components/MainContent.vue';
 import PageFooter from './components/PageFooter.vue';
 
 export default {
   components: {
     PageHeader,
-    Product,
+    MainContent,
     PageFooter,
   },
   data() {
@@ -17,17 +17,7 @@ export default {
 
 <template>
   <PageHeader />
-  <main class="main-content">
-    <section>
-      <div class="container">
-        <div class="row">
-          <div v-for="n in 6" :key="n" class="col-4">
-            <Product />
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
+  <MainContent />
   <PageFooter />
 </template>
 
---
+++ src/components/MainContent.vue
@@ -0,0 +1,33 @@
+<script>
+import Product from './Product.vue';
+import garments from '../db.json';
+
+export default {
+    components: { Product },
+    data() {
+        return
+    }
+}
+
+
+</script>
+
+<template>
+    <main class="main-content">
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div v-for="n in 6" :key="n" class="col-4">
+                        <Product />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+section {
+    padding: 35px;
+}
+</style>
